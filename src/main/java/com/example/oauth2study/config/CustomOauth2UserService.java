b16eb8dd46c08eb15d8925f027e1cd3e3dadf908
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-01-15 21:39:42 +0900

    [FEAT] : SpringSecurity CustomOauth2UserService 설정 추가
    - Resource 서버로부터 받은 user_info 정보를 확인하기 위한 로그 출력

---
+++ src/main/java/com/example/oauth2study/config/CustomOauth2UserService.java
@@ -0,0 +1,27 @@
+package com.example.oauth2study.config;
+
+import com.example.oauth2study.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class CustomOauth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    public CustomOauth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        log.info("oauth2User = {}", oAuth2User.getAttributes());
+        return oAuth2User;
+    }
+}
--- src/main/java/com/example/oauth2study/config/SpringSecurityConfig.java
+++ src/main/java/com/example/oauth2study/config/SpringSecurityConfig.java
@@ -1,19 +1,24 @@
 package com.example.oauth2study.config;
 
 import com.example.oauth2study.user.model.Role;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-@RequiredArgsConstructor
 public class SpringSecurityConfig {
 
+    private final CustomOauth2UserService customOauth2UserService;
+
+    public SpringSecurityConfig(CustomOauth2UserService customOauth2UserService) {
+        this.customOauth2UserService = customOauth2UserService;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -35,7 +40,10 @@ public class SpringSecurityConfig {
                     .baseUri("/oauth2/authorization") //로그인페이지를 받기위한 서버의 엔드포인트 설정
                 .and()
                     .redirectionEndpoint()
-                    .baseUri("/*/oauth2/code/*");
+                    .baseUri("/*/oauth2/code/*")
+                .and()
+                    .userInfoEndpoint()
+                    .userService(customOauth2UserService);
         return http.build();
     }
 }
