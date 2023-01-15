0894a1d3e0062aa9bc9d28f033bb4f9c40fff0cc
박세준 <tpwns6693@gmail.com>
2023-01-16 02:09:22 +0900

    [FEAT] : OAuth2User, UserDetails를 구현한 CustomUserPrincipal 생성
    - 사용자의 계정 정보를 담고 있는 UserDetails, OAuth2 사용자의 정보를 담고 있는 OAuth2User 인터페이스를 구현한 CustomUserPrincipal 생성
    - OAuth2User를 return 하는 Oauth2UserService에서 CustomUserPrincipal을 생성하고, 반환하도록 코드 작성

--- src/main/java/com/example/oauth2study/auth/CustomOauth2UserService.java
+++ src/main/java/com/example/oauth2study/auth/CustomOauth2UserService.java
@@ -1,9 +1,12 @@
 package com.example.oauth2study.auth;
 
+import com.example.oauth2study.auth.model.CustomUserPrincipal;
+import com.example.oauth2study.user.model.ProviderType;
 import com.example.oauth2study.user.model.Role;
 import com.example.oauth2study.user.model.User;
 import com.example.oauth2study.user.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -17,32 +20,41 @@ import java.util.Optional;
 public class CustomOauth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
-
-    public CustomOauth2UserService(UserRepository userRepository) {
+    private final PasswordEncoder passwordEncoder;
+    public CustomOauth2UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
         log.info("oauth2User = {}", oAuth2User.getAttributes());
-
-        String nickName = oAuth2User.getAttributes().get("name").toString();
+        // TODO: 2023-01-16 너무 비대한 로직을 지니고 있어서 나중에 private 메소드로 분리 해야 할 듯
+        String name = oAuth2User.getAttributes().get("name").toString();
         String email = oAuth2User.getAttributes().get("email").toString();
-        String provider = userRequest.getClientRegistration().getRegistrationId();
+        String password = passwordEncoder.encode(email);
+        ProviderType provider = ProviderType.valueOf(userRequest.getClientRegistration().getRegistrationId().toUpperCase());
         String providerId = oAuth2User.getName();
-
         User user = null;
         Optional<User> findUser = userRepository.findByEmail(email);
         if (findUser.isEmpty()) {
             log.info("최초 로그인입니다. 회원가입 진행합니다");
-            user = new User(nickName, email, Role.USER);
+            user = User.builder()
+                    .password(password)
+                    .role(Role.USER)
+                    .email(email)
+                    .name(name)
+                    .provider(provider)
+                    .providerId(providerId)
+                    .build();
             userRepository.save(user);
         } else{
             log.info("구글 로그인 기록이 있습니다.");
-            user = findUser.get().update(nickName);
+            user = findUser.get().update(name);
         }
 
-        return oAuth2User;
+        return new CustomUserPrincipal(user, oAuth2User.getAttributes());
+
     }
 }
---
+++ src/main/java/com/example/oauth2study/auth/model/CustomUserPrincipal.java
@@ -0,0 +1,68 @@
+package com.example.oauth2study.auth.model;
+
+import com.example.oauth2study.user.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class CustomUserPrincipal implements OAuth2User, UserDetails {
+
+    private final User user;
+    private Map<String,Object> attributes;
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(user.getRoleKey()));
+    }
+
+    @Override
+    public String getName() {
+        return user.getName();
+    }
+
+}
