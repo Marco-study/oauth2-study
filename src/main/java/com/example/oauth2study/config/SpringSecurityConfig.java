44c6b7df486b758de1596292c7cdd1ea99b46d99
박세준 <tpwns6693@gmail.com>
2023-01-16 00:51:37 +0900

    [ADD] : PasswordEncoder 추가
    - BCryptPasswordEncoder추가(default가 depreated 되서 직접 만들어 줘야함)

--- src/main/java/com/example/oauth2study/config/SpringSecurityConfig.java
+++ src/main/java/com/example/oauth2study/config/SpringSecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -49,5 +51,9 @@ public class SpringSecurityConfig {
                     .userInfoEndpoint()
                     .userService(customOauth2UserService);
         return http.build();
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
