fdd855c6346b0139fe8a3a342219ff68142ec4f6
Lee Suyoung <78334910+twotwobread@users.noreply.github.com>
2023-01-15 23:28:08 +0900

    [FEAT] : CustomOauth2UserService loadUser 로직 추가.

--- src/main/java/com/example/oauth2study/auth/CustomOauth2UserService.java
+++ src/main/java/com/example/oauth2study/auth/CustomOauth2UserService.java
@@ -1,5 +1,7 @@
-package com.example.oauth2study.config;
+package com.example.oauth2study.auth;
 
+import com.example.oauth2study.user.model.Role;
+import com.example.oauth2study.user.model.User;
 import com.example.oauth2study.user.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
@@ -7,6 +9,8 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Slf4j
 @Component
@@ -22,6 +26,23 @@ public class CustomOauth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
         log.info("oauth2User = {}", oAuth2User.getAttributes());
+
+        String nickName = oAuth2User.getAttributes().get("name").toString();
+        String email = oAuth2User.getAttributes().get("email").toString();
+        String provider = userRequest.getClientRegistration().getRegistrationId();
+        String providerId = oAuth2User.getName();
+
+        User user = null;
+        Optional<User> findUser = userRepository.findByEmail(email);
+        if (findUser.isEmpty()) {
+            log.info("최초 로그인입니다. 회원가입 진행합니다");
+            user = new User(nickName, email, Role.USER);
+            userRepository.save(user);
+        } else{
+            log.info("구글 로그인 기록이 있습니다.");
+            user = findUser.get().update(nickName);
+        }
+
         return oAuth2User;
     }
 }
